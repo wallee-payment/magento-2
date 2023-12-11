d38cbd5563f1aaee41a2149be9235e6c59c4d873
Edgaras <edgaras.tkacenka@wallee.com>
2023-12-11 12:38:37 +0200

    Release 2.1.8

--- view/adminhtml/web/js/label-container.js
+++ view/adminhtml/web/js/label-container.js
@@ -1,11 +1,12 @@
 /**
- * wallee Magento 2
+ wallee Magento 2
  *
- * This Magento 2 extension enables to process payments with wallee (https://www.wallee.com/).
+ * This Magento 2 extension enables to process payments with wallee (https://www.wallee.com).
  *
  * @package Wallee_Payment
- * @author wallee AG (http://www.wallee.com/)
+ * @author wallee AG (https://www.wallee.com)
  * @license http://www.apache.org/licenses/LICENSE-2.0  Apache Software License (ASL 2.0)
+
  */
 require([
     'jquery',
